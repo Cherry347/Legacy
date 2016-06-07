fbf67a4d24a0db54d066d3eaec8abd63e6c09a2d
cherry347 <StillLoriLeigh@gmail.com>
2016-06-07 22:04:51 +0000

    update

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -183,16 +183,17 @@ function signIn (req, res, next) {
 			return res.json({message: "user doesn't exist"});
 		}
 	});
-passport.authenticate('local', function(err, user, info) {
-        if (err) { return next(err); }
-        if (!user) { return res.send({error : 'something went wrong :('}); }
-        req.logIn(user, function(err) {
-            if (err) { return next(err); }
-            return res.send({success:'success'});
-        });
-    })
-    (req, res, next);
 }
+// passport.authenticate('local', function(err, user, info) {
+//         if (err) { return next(err); }
+//         if (!user) { return res.send({error : 'something went wrong :('}); }
+//         req.logIn(user, function(err) {
+//             if (err) { return next(err); }
+//             return res.send({success:'success'});
+//         });
+//     })
+//     (req, res, next);
+// }
 
 
 app.get('/api/users', app.isAuthenticated, function(req, res){

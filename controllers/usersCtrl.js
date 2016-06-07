70732b4bd9207b47991f986d3aba3f7131f4d4cd
cherry347 <StillLoriLeigh@gmail.com>
2016-06-07 19:37:55 +0000

    update

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,5 +1,10 @@
-var User    = require('../models/users.js');
-var s3      = require('s3');
+var User    	= require('../models/users.js');
+var s3      	= require('s3');
+var bcrypt  	= require('bcryptjs');
+var passport    = require('passport');
+var express 	= require('express');
+var app 		= express();
+
 
 var s3Client = s3.createClient({
 	s3Options :{
@@ -11,19 +16,44 @@ var s3Client = s3.createClient({
 
 function createUser(req, res) {
 
-	var file = req.files.files;
+	// var file = req.files.files;
 
 
 
-	// Initiate the upload
-	var uploader = s3Client.uploadFile({
-		localFile: file.path,
-		s3Params: {
-			Bucket: 'legacyphotoalbum',
-			Key: '/legacyphotoalbum/' + file.name,
-			ACL: 'public-read'
-		}
-	});
+	// // Initiate the upload
+	// var uploader = s3Client.uploadFile({
+	// 	localFile: file.path,
+	// 	s3Params: {
+	// 		Bucket: 'legacyphotoalbum',
+	// 		Key: '/legacyphotoalbum/' + file.name,
+	// 		ACL: 'public-read'
+	// 	}
+	// });
+
+	    bcrypt.genSalt(11, function(error, salt){
+        bcrypt.hash(req.body.password, salt, function(hashError, hash){
+            var newUser = new User({
+                username: req.body.username,
+                password: hash,
+            });
+            newUser.save(function(saveErr, user){
+                if ( saveErr ) { res.send({ err:saveErr }) }
+                else {
+                    req.login(user, function(loginErr){
+                        if ( loginErr ) { res.send({ err:loginErr }) }
+                        else { res.send({success: 'success'}) }
+                    })
+                }
+            })
+        })
+    })
+
+
+
+
+
+
+
 
 
 
@@ -48,38 +78,60 @@ function createUser(req, res) {
 
 }
 
-function signIn (req, res) {
-	User.findOne({userName : req.body.userName}, function(err, user) {
-		if(err) {
-			res.json(err);
-		}
-		if(user) {
-				res.json({sucsess: true, message: "you're logged in!", user: user});
-			}
-		else {
-			res.json({message: "user doesn't exist"});
-		}
-	})
+function signIn (req, res, next) {
+// 	User.findOne({userName : req.body.userName}, function(err, user) {
+// 		if(err) {
+// 			res.json(err);
+// 		}
+// 		if(user) {
+// 				res.json({sucsess: true, message: "you're logged in!", user: user});
+// 			}
+// 		else {
+// 			res.json({message: "user doesn't exist"});
+// 		}
+// 	})
+// }
+
+
+passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.send({error : 'something went wrong :('}); }
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.send({success:'success'});
+        });
+    })(req, res, next);
 }
 
+app.get('/users', app.isAuthenticated, function(req, res){
+    res.sendFile('/users/:id', {root: './public'});
+});
 
-function getUsers (req, res){
-	console.log('params', req.params);
-	if(req.params.userID){
-		User.findOne({_id : req.params.userID})
-			.exec(function(req, res){
-			});
-	}
+app.get('/api/me', app.isAuthenticatedAjax, function(req, res){
+    res.send({user:req.user});
+});
+
+// Stupid simple err catcher
+app.use(function(req, res){
+    res.send({err : 'Something bad happened'});
+});
 
 
 
-	// Get MANY
-	else{
-		User.find({}, function(err, docs){
-			res.send(docs);
-		});
-	}
-}
+
+
+// function getUsers (req, res){
+// 	console.log('params', req.params);
+// 	if(req.params.userID){
+// 		User.findOne({_id : req.params.userID})
+// 			.exec(function(req, res){
+// 			});
+// 	}
+
+
+
+
+
 
 function updateUser (req, res){
 		User.update({_id : req.params.userID}, req.body, function(err, updated){
@@ -94,7 +146,6 @@ function updateUser (req, res){
 
 module.exports = {
 	createUser : createUser,
-	getUsers : getUsers,
 	updateUser : updateUser,
 	signIn: signIn
 };
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var logger      = require('morgan');
-var mongoose    = require('mongoose');
-var multiparty  = require('connect-multiparty');
-var usersCtrl   = require('./controllers/usersCtrl.js');
-var fs          = require("fs");
-var https       = require("https");
+var express         = require('express');
+var bodyParser      = require('body-parser');
+var logger          = require('morgan');
+var mongoose        = require('mongoose');
+var multiparty      = require('connect-multiparty');
+var usersCtrl       = require('./controllers/usersCtrl.js');
+var User            = require('./models/users.js');
+var fs              = require("fs");
+var https           = require("https");
+var passport        = require('passport');
+var bcrypt          = require('bcryptjs');
+var LocalStrategy   = require('passport-local').Strategy;
+
 
 var app = express();
 
@@ -19,14 +24,94 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
+/** Express Session Setup **/
+var session = require('express-session');
+app.sessionMiddleware = session({
+  secret: process.env.PASSSECRET,
+  resave: false,
+  saveUninitialized: true,
+});
+app.use(app.sessionMiddleware);
+
+
+/** End Express Session Setup **/
+
+
+
+/** Passport Config **/
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false);
+            }
+
+            bcrypt.compare(password, user.password, function(error, matched){
+                if (matched === true){
+                    return done(null,user);
+                }
+                else {
+                    return done(null, false);
+                }
+            });
+        });
+    }
+));
+
+
+
+/** End Passport Config **/
+
+
+
+
+/** Middleware **/
+app.isAuthenticated = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    console.log('get outta here!');
+    res.redirect('/');
+};
+
+
+app.isAuthenticatedAjax = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.send({error:'not logged in'});
+};
+
+
+
+/** END Middleware **/
+
+
 // Routes \\
 
 //SignIn\\
 app.post('/api/signIn', usersCtrl.signIn);
 
-// GET
-app.get('/api/users', usersCtrl.getUsers);
-app.get('/api/users/:userID', usersCtrl.getUsers);
+// // GET
+// app.get('/api/users', usersCtrl.getUsers);
+// app.get('/api/users/:userID', usersCtrl.getUsers);
 
 
 //POST
@@ -44,7 +129,7 @@ app.listen(port, function(){
 
 
 
-})
+});
 
 
 // HTTPS Setup //
@@ -58,7 +143,7 @@ try {
     https.createServer(credentials, app).listen(443);
 }
 catch(error){
-    console.log('HTTPS setup failed.')
-    console.log('=-=-=-=-=-=-=-=-=-=-=')
-    console.log(error)
+    console.log('HTTPS setup failed.');
+    console.log('=-=-=-=-=-=-=-=-=-=-=');
+    console.log(error);
 }
